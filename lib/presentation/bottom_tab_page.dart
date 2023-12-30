d05a4fe7dc07180e37679c10e0a2dd20bce1a9d6
koby0203 <ms23opboc@gmail.com>
2023-12-30 10:18:56 +0900

    bottomタブと各ページのひな形作成

---
+++ lib/presentation/bottom_tab_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:my_recipe/presentation/favorite_page.dart';
+import 'package:my_recipe/presentation/home_page.dart';
+import 'package:my_recipe/presentation/post_page.dart';
+
+class BottomTabPage extends StatefulWidget {
+  const BottomTabPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _BottomTabPageState();
+  }
+}
+
+class _BottomTabPageState extends State<BottomTabPage> {
+
+  int _currentIndex = 0;
+  final _pageWidgets = [
+    const HomePage(),
+    const FavoritePage(),
+    const PostPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pageWidgets.elementAt(_currentIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'ホーム'),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'お気に入り'),
+          BottomNavigationBarItem(icon: Icon(Icons.upload), label: '投稿'),
+        ],
+        currentIndex: _currentIndex,
+        fixedColor: Colors.blueAccent,
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed,
+      ),
+    );
+  }
+
+  void _onItemTapped(int index) => setState(() => _currentIndex = index );
+}
